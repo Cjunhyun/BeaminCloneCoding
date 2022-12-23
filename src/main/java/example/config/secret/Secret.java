e9bad28fa5f07bf984d3c1d32258257716d19149
Cjunhyun <48709581+Cjunhyun@users.noreply.github.com>
2022-12-23 19:45:05 +0900

    Update Secret.java

--- src/main/java/example/config/secret/Secret.java
+++ src/main/java/example/config/secret/Secret.java
@@ -1,6 +1,6 @@
 package example.config.secret;
 
 public class Secret {
-    public static String JWT_SECRET_KEY = "gachonumcserverclonecoding~";  // 내용을 임의로 바꿔주세요
+    public static String JWT_SECRET_KEY = "";  // 내용을 임의로 바꿔주세요
   
 }
